b2c4ae1d0ecc6130c3983b2079ba82d84698d2cc
Pedro K. L <pedro.karasudani@hotmail.com>
2023-08-25 21:28:28 -0300

    SPRING - Botão para exclusão

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,12 @@ public class FilmeController {
         Filme filme = new Filme(dados);
         repository.save(filme);
 
+        return "redirect:/filmes";
+    }
+ 
+    @DeleteMapping
+    public String removeFilme(){
+        System.out.println("excluido");
         return "redirect:/filmes";
     }
 }
